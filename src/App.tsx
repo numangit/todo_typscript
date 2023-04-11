7117ade7e5137d6c4920f667b3c1cbef38b4e1f4
numangit <abdulnumanjob@gmail.com>
2023-04-12 00:45:56 +0600

    typscript added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { useState } from 'react'
 import './App.css'
+import InputField from './components/InputField'
+
+interface Task {
+  id: number;
+  taskName: string;
+  status: boolean;
+}[]
 
 function App() {
+  const [task, setTask] = useState<Task>();
   return (
-    <div>
+    <div className='flex items-center justify-center bg-slate-900 h-screen'>
+      <div className='w-1/2'>
+        <h1 className='text-5xl text-center text-white font-bold mb-5'>Todo List</h1>
+        <InputField setTask={setTask} />
+      </div>
     </div>
   )
 }
