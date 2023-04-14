62660221aeba925d2626b1673e4736e3a88b85ec
numangit <abdulnumanjob@gmail.com>
2023-04-14 07:45:45 +0600

    AddTodo component added

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,12 @@
+type Props = {}
+
+const AddTodo = (props: Props) => {
+    return (
+        <form>
+            <label htmlFor="Title">Title</label>
+            <input type="text" />
+        </form>
+    )
+}
+
+export default AddTodo
