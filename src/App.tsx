20cd2c0101267524a028cce91c3b05258d9ee151
numangit <abdulnumanjob@gmail.com>
2023-04-14 07:56:59 +0600

    Add todo input added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import InputField from './components/InputField'
 import Todos from './components/Todos';
 import { todoData } from './data/data';
-
-interface Task {
-  id: number;
-  taskName: string;
-  status: boolean;
-}[]
+import AddTodo from './components/AddTodo';
 
 function App() {
 
-  const [task, setTask] = useState<Task>();
   const [todos, setTodos] = useState(todoData);
 
   //function to delete
@@ -25,7 +18,7 @@ function App() {
     <div className='flex items-center justify-center bg-slate-900 h-screen'>
       <div className='w-1/2'>
         <h1 className='text-5xl text-center text-white font-bold mb-5'>Todo List</h1>
-        <InputField setTask={setTask} />
+        <AddTodo />
         <Todos todos={todos} handleDeleteTodo={handleDeleteTodo} />
       </div>
     </div>
--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,10 +1,34 @@
+import { useState } from "react"
+
 type Props = {}
 
 const AddTodo = (props: Props) => {
+    const [title, setTitle] = useState("");
+
     return (
         <form>
-            <label htmlFor="Title">Title</label>
-            <input type="text" />
+            <div className="relative flex">
+                {/* <label htmlFor="Title" className="text-slate-50 font-semibold">Add Todo</label> */}
+                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required autoFocus placeholder="Add Todo" className="w-full rounded-md bg-slate-700 border-gray-200 py-2.5 pr-10 shadow-sm sm:text-sm text-white" />
+                <span className="absolute inset-y-0 right-0 grid w-10 place-content-center">
+                    <button
+                        type="button"
+                        className="rounded-full bg-rose-600 p-0.5 text-white hover:bg-rose-700"
+                    >
+                        <span className="sr-only">Submit</span>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="h-4 w-4"
+                        >
+                            <path
+                                d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"
+                            />
+                        </svg>
+                    </button>
+                </span>
+            </div>
         </form>
     )
 }
